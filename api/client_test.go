b4785b59ce1aa413c6ab13086cb3cb4a3575fb37
Steven Sklar <sklarsa@gmail.com>
2016-11-25 20:21:46 -0500

    Adds error logging to testing

--- api/client_test.go
+++ api/client_test.go
@@ -22,16 +22,22 @@ func TestMain(m *testing.M) {
 func RunTests(m *testing.M) int {
 
 	log.Println("Creating test exchange and queue...")
-	conn, _ := amqp.Dial(os.Getenv("AMQP_URL"))
+	conn, err := amqp.Dial(os.Getenv("AMQP_URL"))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 
-	channel, _ := conn.Channel()
+	channel, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
 	channel.ExchangeDeclare("test_exchange", "topic", true, false, false, false, amqp.Table{})
 	channel.QueueDeclare("test_queue", true, false, false, false, amqp.Table{})
 	channel.QueueBind("test_queue", "#", "test_exchange", false, amqp.Table{})
 
 	log.Println("Exchange and queue created.  Running unit tests...")
 
-	defer conn.Close()
 	return m.Run()
 
 }
@@ -39,98 +45,155 @@ func RunTests(m *testing.M) int {
 func TestClient(t *testing.T) {
 
 	c := Client{MgmtUrl: mgmtUrl, Username: username, Password: password}
-	overview, _ := c.GetOverview()
+	overview, err := c.GetOverview()
 	if reflect.DeepEqual(overview, Overview{}) {
 		t.Errorf("Result from api/overview is nil")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	extensions, _ := c.GetExtensions()
+	extensions, err := c.GetExtensions()
 	if len(extensions) == 0 {
 		t.Errorf("Result from api/extensions has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	nodes, _ := c.GetNodes()
+	nodes, err := c.GetNodes()
 	if len(nodes) == 0 {
 		t.Errorf("Result from api/nodes has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	node, _ := c.GetNode("rabbit@localhost")
+	node, err := c.GetNode("rabbit@localhost")
 	if reflect.DeepEqual(node, Node{}) {
 		t.Errorf("Result from api/node/rabbit@localhost is nil")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	connections, _ := c.GetConnections()
+	connections, err := c.GetConnections()
 	if len(connections) == 0 {
 		t.Errorf("Result from api/connections has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	connections, _ = c.GetConnectionsOnVhost("/")
+	connections, err = c.GetConnectionsOnVhost("/")
 	if len(connections) == 0 {
 		t.Errorf("Result from api/vhosts/vhost/connections has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	channels, _ := c.GetChannels()
+	channels, err := c.GetChannels()
 	if len(channels) == 0 {
 		t.Errorf("Result from api/channels has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	channels, _ = c.GetChannelsOnVhost("/")
+	channels, err = c.GetChannelsOnVhost("/")
 	if len(channels) == 0 {
 		t.Errorf("Result from api/vhosts/vhost/channels has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	channels, _ = c.GetChannelsOnConnection(connections[0].Name)
+	channels, err = c.GetChannelsOnConnection(connections[0].Name)
 	if len(channels) == 0 {
 		t.Errorf("Result from api/connections/connection/channels has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	exchanges, _ := c.GetExchanges()
+	exchanges, err := c.GetExchanges()
 	if len(exchanges) == 0 {
 		t.Errorf("Result from api/exchanges has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	exchangesOnVhost, _ := c.GetExchangesOnVhost("/")
+	exchangesOnVhost, err := c.GetExchangesOnVhost("/")
 	if len(exchangesOnVhost) == 0 {
 		t.Errorf("Result from api/exchanges/vhost on default vhost has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	bindings, _ := c.GetBindings()
+	bindings, err := c.GetBindings()
 	if len(bindings) == 0 {
 		t.Errorf("Result from api/bindings has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	bindings, _ = c.GetBindingsOnVhost("/")
+	bindings, err = c.GetBindingsOnVhost("/")
 	if len(bindings) == 0 {
 		t.Errorf("Result from api/bindings/vhost has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	vhosts, _ := c.GetVhosts()
+	vhosts, err := c.GetVhosts()
 	if len(vhosts) == 0 {
 		t.Error("Result from api/vhosts has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	permissions, _ := c.GetVhostPermissions("/")
+	permissions, err := c.GetVhostPermissions("/")
 	if len(permissions) == 0 {
 		t.Error("Result from api/vhost/name/permissions has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	users, _ := c.GetUsers()
+	users, err := c.GetUsers()
 	if len(users) == 0 {
 		t.Error("Result from api/users has no values")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	status, _ := c.GetAlivenessTestForVhost("/")
+	status, err := c.GetAlivenessTestForVhost("/")
 	if reflect.DeepEqual(status, Status{}) {
 		t.Error("Result from api/aliveness-test/vhost is nil")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	status, _ = c.GetHealthcheckForCurrentNode()
+	status, err = c.GetHealthcheckForCurrentNode()
 	if reflect.DeepEqual(status, Status{}) {
 		t.Error("Result from api/healthchecks/node is nil")
 	}
+	if err != nil {
+		log.Panic(err)
+	}
 
-	status, _ = c.GetHealthchecksForNode("rabbit@localhost")
+	status, err = c.GetHealthchecksForNode("rabbit@localhost")
 	if reflect.DeepEqual(status, Status{}) {
 		t.Error("Result from api/healthchecks/node/node is nil")
+	}
+	if err != nil {
+		log.Panic(err)
 	}
 }
